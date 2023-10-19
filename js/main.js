e21870b5a546a2d43fd2c0ace3b050905ff69eee
Dale Willemse <DaleWillemse000@gmail.com>
2023-10-19 18:48:17 +0200

    Post flight procedures section

---
+++ js/main.js
@@ -0,0 +1,44 @@
+function showShutdown() {
+  var x = document.getElementById("infoShutdown");
+  var y = document.getElementById("arrowImgShutdown");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showSecure() {
+  var x = document.getElementById("infoSecure");
+  var y = document.getElementById("arrowImgSecure");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showDamage() {
+  var x = document.getElementById("infoDamage");
+  var y = document.getElementById("arrowImgDamage");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showDocumentation() {
+  var x = document.getElementById("infoDocumentation");
+  var y = document.getElementById("arrowImgDocumentation");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
