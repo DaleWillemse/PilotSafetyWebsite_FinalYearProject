8565594116113c02e597533cc9b5d90e005f9b85
danaemarais <80810274+danaemarais@users.noreply.github.com>
2023-10-20 09:49:06 +0200

    Considerations Update
    I added some content under the "Things to Consider" heading.

---
+++ main.js
@@ -0,0 +1,88 @@
+function showShutdown() {
+  var x = document.getElementById("infoShutdown");
+  var y = document.getElementById("arrowImgShutdown");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showSecure() {
+  var x = document.getElementById("infoSecure");
+  var y = document.getElementById("arrowImgSecure");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showDamage() {
+  var x = document.getElementById("infoDamage");
+  var y = document.getElementById("arrowImgDamage");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showDocumentation() {
+  var x = document.getElementById("infoDocumentation");
+  var y = document.getElementById("arrowImgDocumentation");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showWeather() {
+  var x = document.getElementById("infoWeather");
+  var y = document.getElementById("arrowImgWeather");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showEmergency() {
+  var x = document.getElementById("infoEmergency");
+  var y = document.getElementById("arrowImgEmergency");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showFitness() {
+  var x = document.getElementById("infoFitness");
+  var y = document.getElementById("arrowImgFitness");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
+function showCompliance() {
+  var x = document.getElementById("infoCompliance");
+  var y = document.getElementById("arrowImgCompliance");
+  if (x.style.display === "block") {
+    y.style.rotate = "360deg";
+    x.style.display = "none";
+  } else {
+    y.style.rotate = "90deg";
+    x.style.display = "block";
+  }
+}
